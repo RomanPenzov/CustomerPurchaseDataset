cea3d73840ecd886939f886bec6d989621907fc7
Roman Penzov <romanpenzov@yandex.ru>
2024-12-14 21:56:35 +0300

    add kafka

---
+++ postgres/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE customer_purchase_data (
+    Number INT PRIMARY KEY,
+    Age INT,
+    Income DECIMAL,
+    Spending_Score DECIMAL,
+    Membership_Years INT,
+    Purchase_Frequency DECIMAL,
+    Last_Purchase_Amount DECIMAL
+);
